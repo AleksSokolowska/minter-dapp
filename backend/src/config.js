e36728839da52598b5c0947cbde595d5dcc6ef08
AleksKlar <aleks@sokolowska.de>
2022-07-01 18:53:24 +0200

    whitelist update

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xe4776Ba3e7183833A86eC57cfdFFc7be86Ce8C05"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xe4776Ba3e7183833A86eC57cfdFFc7be86Ce8C05"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
