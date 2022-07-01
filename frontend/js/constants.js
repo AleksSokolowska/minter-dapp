1bc734259dcd87871f6d6a3cad5b18231eee2f72
AleksKlar <aleks@sokolowska.de>
2022-07-01 11:40:17 +0200

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0xBc1BeCE7770847D8B037EaC47B4684A0724C0787"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
